be9efc06331435afc7fbd70a99939ac514c6e373
Serena Nguyen <s.nguyen11@utexas.edu>
2019-12-14 20:58:55 -0600

    add footer ad

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,27 +3,39 @@ import React from "react";
 export default () => {
   return (
     <div className="footer">
-      <a
-        href="mailto:s.nguyen11@utexas.edu"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="far fa-envelope" />
-      </a>
-      <a
-        href="https://www.instagram.com/serenanguyen_/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fab fa-instagram" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/serenanguyenatx/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fab fa-linkedin" />
-      </a>
+      <div id="ad-container">
+        <div className="greenbelt-logo" />
+        <a
+          href="https://www.davidrjameson.com/greenbelt-patch/purchase-greenbelt-patch"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p>Shop locally designed Greenbelt Goods!</p>
+        </a>
+      </div>
+      <div className="button-container">
+        <a
+          href="mailto:snguyenatx@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="far fa-envelope" />
+        </a>
+        <a
+          href="https://www.instagram.com/serenanguyen_/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-instagram" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/serenanguyenatx/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin" />
+        </a>
+      </div>
     </div>
   );
 };
